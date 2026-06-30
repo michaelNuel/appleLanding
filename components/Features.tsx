f3b5fac4f1abec5ebf402e3e1efa48b3d66a50e7
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2026-06-30 19:47:53 +0100

    added 3d spin to features page

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,6 +4,7 @@ import StudioLights from "./three/StudioLights"
 import { features, featureSequence } from "@/constant"
 import clsx from "clsx"
 import { Suspense, useEffect, useRef } from "react"
+import { Group } from "three"
 import { Html } from "@react-three/drei"
 import MacbookModel from "./models/Macbook"
 import { useMediaQuery } from "react-responsive"
@@ -12,64 +13,88 @@ import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 
 const ModelScroll = () => {
-    const groupRef = useRef(null) 
-    const isMobile = useMediaQuery({ query: '(max-width:1024px)'})
-    const {setTexture} = useMacBookStore()
+  const groupRef = useRef<Group>(null)
+  const isMobile = useMediaQuery({ query: '(max-width:1024px)' })
+  const { setTexture } = useMacBookStore()
 
-    //Preload all future videos during component mount 
-    useEffect(() => {
-      featureSequence.forEach((feature) => {
-        const v = document.createElement('video');
-        Object.assign(v, {
-          src: feature.videoPath, 
-          muted: true, 
-          playsInline: true,
-          preload: 'auto',
-          crossOrigin: 'anonymous',
-      
-        })
+  //Preload all future videos during component mount 
+  useEffect(() => {
+    featureSequence.forEach((feature) => {
+      const v = document.createElement('video');
+      Object.assign(v, {
+        src: feature.videoPath,
+        muted: true,
+        playsInline: true,
+        preload: 'auto',
+        crossOrigin: 'anonymous',
 
-        //append video to DOM
-        v.load()
-        
       })
+
+      //append video to DOM
+      v.load()
+
+    })
+  })
+
+  useGSAP(() => {
+    //3D model Rotation animation
+    const modelTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#f-canvas',
+        start: 'top top', 
+        end: 'bottom top', 
+        scrub: 1, 
+        pin: true
+      }
+
     })
 
-    useGSAP(() => {
-      //3D model Rotation animation
-      const modelTimeline = gsap
+    //Sync the feature content
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#f-canvas', 
+        start: 'top center', 
+        end: 'bottom top', 
+        scrub: 1, 
+      }
     })
-    return (
-        <group ref={groupRef}>
-            <Suspense fallback={<Html>
-                <h1 className="text-white text-3xl uppercase">
-                  loading....
-                </h1> 
-            </Html>}>
-              <MacbookModel scale={isMobile ? 0.05 : 0.08} position={[0, -1, 0]} />
-            </Suspense>
-        </group>
-    )
+
+    //3d Spin
+    if(groupRef.current) {
+      modelTimeline.to(groupRef.current.rotation, {y: Math.PI * 2, ease: 'power1.inOut'})
+    }
+  })
+  return (
+    <group ref={groupRef}>
+      <Suspense fallback={<Html>
+        <h1 className="text-white text-3xl uppercase">
+          loading....
+        </h1>
+      </Html>}>
+        <MacbookModel scale={isMobile ? 0.05 : 0.08} position={[0, -1, 0]} />
+      </Suspense>
+    </group>
+  )
 }
 
 const Features = () => {
-    return (
-        <section id="features">
-             <h2>See it all in a new light.</h2>
+  return (
+    <section id="features">
+      <h2>See it all in a new light.</h2>
 
-             <Canvas id="f-canvas" camera={{}}>
-               <StudioLights />
-               <ambientLight intensity={0.5} />
-               <ModelScroll />
-             </Canvas>
+      <Canvas id="f-canvas" camera={{}}>
+        <StudioLights />
+        <ambientLight intensity={0.5} />
+        <ModelScroll />
+      </Canvas>
 
-             <div className="absolute inset-0">
-               {features.map((feature, index) => (
-                <div className={clsx('box', `box${index + 1}`, feature.styles)}>{feature.text}</div>  
-               ))}
-             </div>
-        </section>
-    )
+      <div className="absolute inset-0">
+        {features.map((feature, index) => (
+          <div className={clsx('box', `box${index + 1}`, feature.styles)}>{feature.text}</div>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Features

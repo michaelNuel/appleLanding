d138c6e169450d57ff1ef59605df0e7f5a49ba74
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2026-06-30 20:32:45 +0100

    finished

--- components/Features.tsx
+++ components/Features.tsx
@@ -41,9 +41,9 @@ const ModelScroll = () => {
     const modelTimeline = gsap.timeline({
       scrollTrigger: {
         trigger: '#f-canvas',
-        start: 'top top', 
-        end: 'bottom top', 
-        scrub: 1, 
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
         pin: true
       }
 
@@ -52,17 +52,31 @@ const ModelScroll = () => {
     //Sync the feature content
     const timeline = gsap.timeline({
       scrollTrigger: {
-        trigger: '#f-canvas', 
-        start: 'top center', 
-        end: 'bottom top', 
-        scrub: 1, 
+        trigger: '#f-canvas',
+        start: 'top center',
+        end: 'bottom top',
+        scrub: 1,
       }
     })
 
     //3d Spin
-    if(groupRef.current) {
-      modelTimeline.to(groupRef.current.rotation, {y: Math.PI * 2, ease: 'power1.inOut'})
+    if (groupRef.current) {
+      modelTimeline.to(groupRef.current.rotation, { y: Math.PI * 2, ease: 'power1.inOut' })
     }
+
+    //Content & texture sync 
+    timeline
+      .call(() => setTexture('/videos/feature-1.mp4')) //Alows us to call an additional function 
+      .to('.box1', { opacity: 1, y: 0, delay: 1 })
+      .call(() => setTexture('/videos/feature-2.mp4')) //Alows us to call an additional function 
+      .to('.box2', { opacity: 1, y: 0, })
+      .call(() => setTexture('/videos/feature-3.mp4')) //Alows us to call an additional function 
+      .to('.box3', { opacity: 1, y: 0, })
+      .call(() => setTexture('/videos/feature-4.mp4')) //Alows us to call an additional function 
+      .to('.box4', { opacity: 1, y: 0, })
+      .call(() => setTexture('/videos/feature-5.mp4')) //Alows us to call an additional function 
+      .to('.box5', { opacity: 1, y: 0, })
+
   })
   return (
     <group ref={groupRef}>
@@ -88,9 +102,18 @@ const Features = () => {
         <ModelScroll />
       </Canvas>
 
-      <div className="absolute inset-0">
+      <div className="absolute inset-0"> 
         {features.map((feature, index) => (
-          <div className={clsx('box', `box${index + 1}`, feature.styles)}>{feature.text}</div>
+          <div className={clsx('box', `box${index + 1}`, feature.styles)}>
+            {/* {feature.text} */}
+            <img src={feature.icon} alt={feature.highlight} />
+            <p>
+              <span className="text-white">
+                {feature.highlight}
+              </span>
+              {feature.text}
+            </p>
+          </div>
         ))}
       </div>
     </section>
